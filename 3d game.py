da0a42530ef83f96c6897eb1624c54bda5628201
White656 <74784236+White656@users.noreply.github.com>
2021-01-15 14:00:59 +0300

    Update 3d game.py

--- 3d game.py
+++ 3d game.py
@@ -6,6 +6,7 @@ width = 1200
 height = 800
 polovina_width = width // 2
 polovina_height = height // 2
+FPS_POS = (width - 65, 5)
 
 #  настройки игрока
 position_for_player = (polovina_width // 4, polovina_height - 50)  # начальное положение игрока
@@ -26,8 +27,39 @@ pesok = (244, 164, 96)
 temno_korichnevui = (97, 61, 25)
 temno_oranzhevui = (255, 140, 0)
 
-class Player():
-    pass
+class Player:
+    def __init__(self):
+        self.x, self.y = position_for_player
+        self.angle = vzglyad_for_player
+
+    # используем  property в качестве декоратора, т.е из атрибута класса в метод класса
+    @property
+    def pos(self):
+        return self.x, self.y
+
+    def movement(self):
+        """В этой функции происходит процесс управления, подробнее описанный в презентации"""
+        sin_a = math.sin(self.angle)
+        cos_a = math.cos(self.angle)
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_w]:
+            self.x += scorost_for_player * cos_a
+            self.y += scorost_for_player * sin_a
+        if keys[pygame.K_s]:
+            self.x += -scorost_for_player * cos_a
+            self.y += -scorost_for_player * sin_a
+        if keys[pygame.K_a]:
+            self.x += scorost_for_player * sin_a
+            self.y += -scorost_for_player * cos_a
+        if keys[pygame.K_d]:
+            self.x += -scorost_for_player * sin_a
+            self.y += scorost_for_player * cos_a
+        if keys[pygame.K_LEFT]:
+            self.angle -= 0.02
+        if keys[pygame.K_RIGHT]:
+            self.angle += 0.02
+
+        self.angle %= DOUBLE_PI
 
 pygame.init()
 screen = pygame.display.set_mode((width, height))
@@ -38,4 +70,5 @@ while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             exit()
+    player.movement()
     screen.fill(chern) # вся поверхность в черный
